46dc8f6ba5658a5d568924afe2b7c1dd0d6e9b3f
myriamleb9 <myriamlebbakher@gmail.com>
2026-07-04 15:32:01 +0100

    Asteroid player mechanism

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pygame
 from constants import *
-from logger import log_state 
+from logger import log_state
+from player import Player 
 pygame.init()
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            return
+    clock = pygame.time.Clock()
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
+        log_state()
         pygame.display.flip()
+    
+    
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width: 1280")
